1ffee724f83730523e173764f840621384abe3d1
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-06 21:02:57 +0600

    file created

--- Var-Data-Type/getch()-getche().cpp
+++ Var-Data-Type/getch()-getche().cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
 
+int main()
+{
+    char ch,cha;
+    ch=getch();
+     cout<<ch;
+    cha=getche();
+
+    cout<<ch;
+    return 0;
+}
