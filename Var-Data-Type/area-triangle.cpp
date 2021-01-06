768761a0db9e3af6a52bfdd9d60fd9dd9e52503a
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-06 21:11:47 +0600

    file created

--- Var-Data-Type/area-triangle.cpp
+++ Var-Data-Type/area-triangle.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+#include<math.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int s,a,b,c;
+    float area;
+    cin>>a>>b>>c;
+    s=(a+b+c)/2;
+    area=sqrt(s*(s-a)*(s-b)*(s-c));
+    cout<<area;
+    return 0;
+}
--- Var-Data-Type/user-def.cpp
+++ Var-Data-Type/user-def.cpp
@@ -1,1 +1,14 @@
 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+typedef int a;
+typedef float b;
+int main()
+{
+    a num=10;
+    b num2=3.1416;
+    cout<<num<<endl;
+    cout<<num2;
+    return 0;
+}
