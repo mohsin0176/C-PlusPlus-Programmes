a56da0c5627a9bb61d8e4ad71da0af573a2c447d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-07 21:15:11 +0600

    Operator created

--- Operator/assign.cpp
+++ Operator/assign.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f;
+    cin>>f;
+    a=b=c=d=e=f;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    cout<<f<<endl;
+    return 0;
+}
--- Operator/logic.cpp
+++ Operator/logic.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+
+cout<<((10>20)&&(30<40))<<endl;
+cout<<((10>20)&&(30>40))<<endl;
+cout<<((10>20)&&(30<40))<<endl;
+cout<<((10>20)&&(30<40))<<endl;
+cout<<((10>20)||(30<40))<<endl;
+cout<<((10>20)||(30>40))<<endl;
+cout<<((10>20)||(30<40))<<endl;
+cout<<((10>20)||(30<40))<<endl;
+cout<<!(10>20)<<endl;
+cout<<!(10<20)<<endl;
+    return 0;
+}
