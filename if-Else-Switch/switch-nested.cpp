3d7001fae374b35dbffcee4e300e4e0ab115a729
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-07 23:34:36 +0600

    Operator created

--- if-Else-Switch/switch-nested.cpp
+++ if-Else-Switch/switch-nested.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
 
+int main()
+{
+
+    int a=100;
+    int b=200;
+    switch(a)
+    {
+    case 100:
+        cout<<"In Outer Switch"<<endl;
+        switch(b)
+        {
+        case 200:
+            cout<<"Inner switch"<<endl;
+        }
+    }
+    cout<<a<<endl;
+    cout<<b;
+    return 0;
+}
