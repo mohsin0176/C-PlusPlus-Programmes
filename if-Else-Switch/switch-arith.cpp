a0189d378d70e8ddf0f46cab650c600baf287038
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-07 23:30:01 +0600

    Operator created

--- if-Else-Switch/switch-arith.cpp
+++ if-Else-Switch/switch-arith.cpp
@@ -1,1 +1,28 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cin>>c;
+    int a,b,res;
+    cin>>a>>b;
+    switch(c)
+    {
+    case '+':
+        res=a+b;
+        cout<<res;
+        break;
+    case '-':
+        res=a-b;
+        cout<<res;
+        break;
+    case '*':
+        res=a*b;
+        cout<<res;
+        break;
+    case '/':
+        res=a/b;
+        cout<<res;
+        break;
+    }
+}
