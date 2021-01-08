db3769afca9f1c4614cabc9deea5b269cd7ce1b0
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-08 17:38:50 +0600

    loop created

--- Array/add-2-mat.cpp
+++ Array/add-2-mat.cpp
@@ -1,1 +1,40 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,a[5][5],b[5][5],c[5][5];
+    for(i=0;i<=4;i++)
+    {
+    for(j=0;j<=4;j++)
+    {
+        cin>>a[i][j];
+        cout<<" ";
+    }
+    cout<<endl;
+    }
+    for(i=0;i<=4;i++)
+    {
+    for(j=0;j<=4;j++)
+    {
+        cin>>b[i][j];
+        cout<<" ";
+    }
+     cout<<endl;
+    }
+    for(i=0;i<=4;i++)
+    {
+    for(j=0;j<=4;j++)
+    {
+        c[i][j]=a[i][j]+b[i][j];
+    }
+    }
+    for(i=0;i<=4;i++)
+    {
+    for(j=0;j<=4;j++)
+    {
+        cout<<c[i][j]<<" ";
+    }
+    cout<<endl;
+    }
+    return 0;
+}
