1c12ef3180cafb94dc5b1ffa027ba854e51ddc86
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-08 18:38:58 +0600

    loop created

--- Array/display-2d-arr.cpp
+++ Array/display-2d-arr.cpp
@@ -1,1 +1,22 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,a[5][5];
+    for(i=0;i<=4;i++)
+    {
+        for(j=0;j<=4;j++)
+    {
+        cin>>a[i][j];
+    }
+    }
+    for(i=0;i<=4;i++)
+    {
+        for(j=0;j<=4;j++)
+    {
+        cout<<a[i][j]<<"\t";
+    }
+    cout<<"\n";
+    }
+    return 0;
+}
--- Array/init-matr-2d-arr.cpp
+++ Array/init-matr-2d-arr.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    int a[2][3]={{10,20,30},{40,50,60}};
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cout<<a[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
