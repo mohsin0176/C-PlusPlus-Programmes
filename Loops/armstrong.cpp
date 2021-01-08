54d450f38a9c01c91e5fccce1fbcd4c285b1979d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-08 16:42:47 +0600

    loop created

--- Loops/armstrong.cpp
+++ Loops/armstrong.cpp
@@ -1,1 +1,24 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,x,n,r,sum;
+    cin>>n;
+    x=n;
+    sum=0;
+    while(n!=0)
+    {
+        r=n%10;
+        sum=sum+(r*r*r);
+        n=n/10;
+    }
+    if(sum==x)
+    {
+        cout<<sum<<" "<<"is Armstrong Number";
+    }
+       else
+       {
+           cout<<sum<<" "<<"is Not Armstrong Number";
+       }
+    return 0;
+}
--- Loops/count number of digit.cpp
+++ Loops/count number of digit.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    long int n,c;
+    cin>>n;
+    c=0;
+    while(n!=0)
+    {
+        n=n/10;
+        c=c+1;
+    }
+    cout<<c;
+    return 0;
+}
