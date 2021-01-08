26ee584cfb2d1f3966e1d6de55139c610b6e3d2d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-08 19:01:54 +0600

    loop created

--- Array/lin-se.cpp
+++ Array/lin-se.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num[10],i,pos=-1,value;
 
+    for(i=0;i<10;i++)
+    {
+        cin>>num[i];
+    }
+    cin>>value;
+    for(i=0;i<10;i++)
+    {
+        if(value==num[i])
+        {
+            pos=i+1;
+            break;
+        }
+    }
+    if(pos==-1)
+    {
+        cout<<value<<"Not found";
+    }
+    else
+
+    {
+        cout<<value<<"Found"<<pos<<"location";
+    }
+    return 0;
+}
--- Array/max-min-arr.cpp
+++ Array/max-min-arr.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,arr[5],min,max;
+    for(i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    min=arr[0];
+    max=arr[0];
+    for(i=0;i<5;i++)
+    {
+        if(min>arr[i])
+{
 
+
+            min=arr[i];
+}
+      else if(max>arr[i])
+       {
+
+
+            max=arr[i];
+       }
+    }
+    cout<<min<<endl;
+    cout<<max<<endl;
+    return 0;
+}
