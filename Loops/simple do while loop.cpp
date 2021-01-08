034aa00becae4375e65093b7d7896ca366d7cda5
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-08 15:49:13 +0600

    loop created

--- Loops/simple do while loop.cpp
+++ Loops/simple do while loop.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=1;
+    do
+    {
+        cin>>n;
+    }
+    while(n!=0);
 
+    return 0;
+}
--- Loops/simple while loop.cpp
+++ Loops/simple while loop.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=1;
+    while(n!=0)
+    {
+        cin>>n;
+    }
+    return 0;
+}
