91b302b2651a72ac6bc32da64a4a1f211379652c
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-08 16:54:15 +0600

    loop created

--- Loops/hcf-lcm.cpp
+++ Loops/hcf-lcm.cpp
@@ -1,1 +1,26 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,s,hcf,i,lcm;
+    cin>>a>>b;
+    if(a<b)
+    {
+        s=a;
+    }
+    else
+    {
+        s=b;
+    }
+    for(i=1;i<=s;i++)
+    {
+        if((a%i==0)&&(b%i==0))
+        {
+            hcf=i;
+        }
+    }
+    cout<<hcf<<endl;
+    lcm=(a*b)/hcf;
+    cout<<lcm;
+    return 0;
+}
--- Loops/lcm-hcm.cpp
+++ Loops/lcm-hcm.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,g,hcf,i,lcm;
+    cin>>a>>b;
+    if(a>b)
+    {
+        g=a;
+    }
+    else
+    {
+        g=b;
+    }
+    for(i=g;i<=(a*b);i++)
+    {
+        if((i%a==0)&&(i%b==0))
+        {
+            lcm=i;
+            break;
+        }
+    }
+    cout<<lcm<<endl;
+    hcf=(a*b)/lcm;
+    cout<<hcf<<endl;
+    return 0;
+}
