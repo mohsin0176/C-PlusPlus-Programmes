79c0437ac0125277b26ef513c8650f0a02452af8
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-09 21:54:45 +0600

    function created

--- Functions/fib-by-recur.cpp
+++ Functions/fib-by-recur.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int fibo(int);
+int main()
+{
+    int n,t;
+    cin>>n;
+    t=fibo(n);
+    cout<<n<<t;
+}
+int fibo(int n)
+{
+    if(n==0||n==1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibo(n-1)+fibo(n-2);
+    }
+}
--- Functions/fun-dec-fun-def-at-top.cpp
+++ Functions/fun-dec-fun-def-at-top.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int show(int );
+int main()
+{
+    int a=10;
+    show(a);
+    return 0;
+}
+int show(int x)
+{
+    cout<<x;
+}
