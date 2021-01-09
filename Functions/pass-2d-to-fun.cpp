200f58789a7686207ef53076a2cf3b4ea455dfb4
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-09 20:56:03 +0600

    function created

--- Functions/pass-2d-to-fun.cpp
+++ Functions/pass-2d-to-fun.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<iomanip.h>
+#include<conio.h>
+using namespace std;
+void main()
+{
+    int ar[2][3]={{10,20,30},{40,50,60};
+    display(ar);
 
+}
+void display(int a[2][3])
+{
+    int i,j;
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cout<<a[i][j];
+        }
+        cout<<endl;
+    }
+}
