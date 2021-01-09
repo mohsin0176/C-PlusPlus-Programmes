69c06178e4cc9699cadc85d74372954fa122920f
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-09 22:35:14 +0600

    function created

--- Structure-Union/simple-struc.cpp
+++ Structure-Union/simple-struc.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+struct Distance
+{
+    int feet;
+    int inches;
+};
 
+int main()
+{
+    Distance d1,d2;
+    d1.feet=12;
+    d1.inches=9.5;
+    cin>>d2.feet;
+    cin>>d2.inches;
+    cout<<d1.feet<<endl<<d1.inches<<endl;
+    cout<<d2.feet<<endl<<d2.inches<<endl;
+    return 0;
+}
--- Structure-Union/string-within-struc.cpp
+++ Structure-Union/string-within-struc.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+ const int SIZE=3;
+ struct Book
+ {
+     char name[25];
+     int page;
+     int price;
+ };
+int main()
+{
+    int i;
+    Book b1[SIZE];
+    for(i=0;i<SIZE;i++)
+    {
+        cin>>b1[i].name;
+        cin>>b1[i].age;
+        cin>>b1[i].price;
+    }
+    for(i=0;i<SIZE;i++)
+    {
+        cout<<b1[i].name;
+        cout<<b1[i].page;
+        cout<<b1[i].price;
+    }
 
+return 0;
+}
