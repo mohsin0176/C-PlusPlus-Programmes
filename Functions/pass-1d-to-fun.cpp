f84577dd28d2c4273fb392c389a6b5668e638a8f
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-09 20:51:33 +0600

    function created

--- Functions/pass-1d-to-fun.cpp
+++ Functions/pass-1d-to-fun.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<conio.h>
 
+using namespace std;
+const int SZ=5;
+int display (int a[sz]);
+int main()
+{
+    int ar[SZ],i;
+    for(i=0;i<SZ;i++)
+    {
+        cin>>ar[i];
+    }
+    display(ar);
+  return 0;
+}
+int display(int a[sz])
+{
+    for(int i=0;i<SZ;i++)
+    {
+        cout<<a[i];
+    }
+
+}
