c78958f297d90a0fcc57101ad97049584c18fb05
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-09 21:20:25 +0600

    function created

--- Functions/arg-but-ret.cpp
+++ Functions/arg-but-ret.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+using namespace std;
+float Simp_Int();
+int main()
+{
+    float si;
+    si=Simp_Int();
+    cout<<si;
+    return 0;
+}
+float Simp_Int()
+{
+    float p,r,t,si;
+    cin>>p>>r>>t;
+    si=(p*t*r)/100;
+    return si;
+}
--- Functions/call-add-point.cpp
+++ Functions/call-add-point.cpp
@@ -1,1 +1,19 @@
 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+float Simp_Int(float,float,float);
+int  main()
+{
+    float p,r,t,si;
+    cin>>p>>r>>t;
+    si=Simp_Int(p,r,t);
+    cout<<si;
+    return 0;
+}
+float Simp_Int(float p,float r,float t)
+{
+    float si;
+    si=(p*r*t)/100;
+    return si;
+}
