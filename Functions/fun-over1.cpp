fd32a966fa2cae2abcb17e6e8d41cd4117060bb1
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-09 21:49:33 +0600

    function created

--- Functions/fun-over1.cpp
+++ Functions/fun-over1.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
 
+void repchar(char,int);
+void repchar(char);
+void repchar();
+int main()
+{
+    repchar();
+    repchar('=');
+    repchar('+',30);
+    return 0;
+}
+void repchar(char ch,int n)
+{
+    for(int j=0;j<n;j++)
+    {
+        cout<<ch;
+    }
+    cout<<'\n';
+
+}
+void repchar(char ch)
+{
+    for(int j=0;j<20;j++)
+    {
+        cout<<ch;
+    }
+    cout<<'\n';
+}
+void repchar(char ch,int n)
+{
+    for(int j=0;j<20;j++)
+    {
+        cout<<"*";
+    }
+    cout<<'\n';
+}
