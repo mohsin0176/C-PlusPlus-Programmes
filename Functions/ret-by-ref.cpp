dfba7ad7f3940e984d07f00849828ad0361bd912
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-09 21:13:35 +0600

    function created

--- Functions/ret-by-ref.cpp
+++ Functions/ret-by-ref.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<coino.h>
+using namespace std;
+int &min(int &a,int &b)
+{
+    if(a<b)
+    {
+        return a;
+    }
+    else
 
+    {
+        return b;
+    }
+}
+void main()
+{
+    int x=10;
+    int y=20;
+    cout<<x<<y<<endl;
+    min(x,y);
+    cout<<x<<y<<endl;
+}
