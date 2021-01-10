fc5bf4455c4622dce3365f578bf511bb3c125297
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 21:32:02 +0600

    class created

--- Class-Object/member-fun-out-class.cpp
+++ Class-Object/member-fun-out-class.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+#define PI 3.1416
+class Circle
+{
+    float radius;
+    public:
+        void getRadius();
+        float area();
+        void showRadius();
 
+};
+void Circle::getRadius()
+{
+    cin>>radius;
+}
+float Circle::area()
+{
+    float ar;
+    ar=PI*radius*radius;
+    return ar;
+}
+float Circle::showRadius()
+{
+    cout<<radius;
+}
+void main()
+{
+    Circle c1;
+    c1.getRadius();
+    c1.showRadius();
+    float a=c1.area();
+    cout<<a;
+}
