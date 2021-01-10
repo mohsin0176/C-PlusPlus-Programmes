b773b7ac8f569a40e836bc9a44b3e957ce3af675
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 20:06:57 +0600

    class created

--- Class-Object/class-without-arg.cpp
+++ Class-Object/class-without-arg.cpp
@@ -1,1 +1,30 @@
-
+#include<iostream>
+#include<conio.h>
+class testMax
+{
+private:
+    int val_A,val_B;
+public:
+    void getValue()
+    {
+        cin>>val_A;
+        cin>>val_B;
+    }
+    void checkMax()
+    {
+        if(val_A>val_B)
+        {
+            cout<<"A is max";
+        }
+        else
+        {
+            cout<<"B is max";
+        }
+    }
+};
+void main()
+{
+    testMax tm;
+    tm.getValue();
+    tm.checkMax();
+}
