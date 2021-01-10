3b1e2fb831bf7f6bcb1b0d0626bf4005538fc597
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 20:03:36 +0600

    class created

--- Class-Object/class-with-argument.cpp
+++ Class-Object/class-with-argument.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+#include<stdio.h>
+class stringfun{
+char name[20];
+public:
+    void concatString(char a[],char b[])
+    {
+        strcat(a,b);
+        strcpy(name,a);
+    }
+    void display()
+    {
+        cout<<name;
+    }
 
+};
+void main()
+{
+    char str1[10],str2[10];
+    stringfun sf;
+    gets(str1);
+    gets(str2);
+    sf.concatString(str1,str2);
+    sf.display();
+}
