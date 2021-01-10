0478178dfba5c4b92b264f4fb58746ef7ea06356
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 22:21:48 +0600

    class created

--- Class-Object/global-class-object-local-object.cpp
+++ Class-Object/global-class-object-local-object.cpp
@@ -1,1 +1,26 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+private:
+    int x;
+public:
+    void getX()
+    {
+        cin>>x;
+    }
+    void showX()
+    {
+        cout<<x;
+    }
+};
+A a1;
+void main()
+{
+    A a2;
+    a1.getX();
+    a2.getX();
+    a1.showX();
+    a2.showX();
+}
