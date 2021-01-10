7279ae461dd372b0c9330ca6d9f4852c4b415407
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 21:19:59 +0600

    class created

--- Class-Object/nested-class-public-inner.cpp
+++ Class-Object/nested-class-public-inner.cpp
@@ -1,1 +1,41 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class outer
+{
+    int a;
+public:
+    int b;
+    class inner
+    {
+        int c;
+    public:
+        int d;
+        inner()
+        {
+            c=30;d=40;
+        }
+        void showInner()
+        {
+            cout<<c<<d;
+        }
+    };
+    inner ob1;
+    outer()
+    {
+        a=10;
+        b=10;
+    }
+    void showOuter()
+    {
+        cout<<a<<b<<ob1.d;
+        ob1.showInner();
+    }
+};
+void main()
+{
+    outer ob;
+    outer::inner ob2;
+    ob.showOuter();
+    ob2.showInner();
+}
