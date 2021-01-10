6a0630f589bac5180228fdfb5d03bed895dae9fd
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 20:44:44 +0600

    class created

--- Class-Object/static-data-memeber-fun.cpp
+++ Class-Object/static-data-memeber-fun.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class stat{
 
+int code;
+static int count;
+public:
+    stat()
+    {
+        code=++count;
+    }
+    void showcode()
+    {
+        cout<<code;
+    }
+    static void showcount()
+    {
+        cout<<count;
+    }
+};
+int stat::count;
+void main()
+{
+    stat obj1,obj2;
+    obj1.showcode();
+    obj1.showcount();
+    obj2.showcode();
+    obj2.showcount();
+
+}
