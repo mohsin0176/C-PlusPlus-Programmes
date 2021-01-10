c8bc62a7abe72041bf7cba6784e346651e455e22
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 21:10:30 +0600

    class created

--- Class-Object/pass-obj-to mem-fun.cpp
+++ Class-Object/pass-obj-to mem-fun.cpp
@@ -1,1 +1,39 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    void getDist()
+    {
+        cin>>feet>>inches;
+    }
+    void showDist()
+    {
+        cout<<feet<<inches;
+    }
+    void addDist(Distance,Distanec);
+};
+void Distance::addDist(Distance dd1,Distance dd2)
+{
+    feet=dd1.feet+dd2.feet;
+    inches=dd1.inches+dd2.inches;
+    if(inches>=12)
+    {
+        inches-=12;
+        feet++;
+    }
+}
+void main()
+{
+    Distance d1,d2,d3;
+    d1.getDist();
+    d2.getDist();
+    d3.addDist(d1,d2);
+    d1.showDist();
+    d2.showDist();
+    d3.showDist();
+}
--- Class-Object/pass-object-to-func.cpp
+++ Class-Object/pass-object-to-func.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+class Circle
+{
+    float radius;
+public:
+    void setRadius();
+    void showRadius();
+    float getRadius();
 
+};
+void Circle::setRadius()
+{
+    cin>>radius;
+}
+void Circle::showRadius()
+{
+    cout<<radius;
+}
+void Circle::getRadius()
+{
+    return radius;
+}
+float calArea(Circle c)
+{
+    float ar;
+    ar=PI*c.getRadius()*c.getRadius();
+    return ar;
+
+}
+
+void main()
+{
+    Circle c1;
+    c1.setRadius();
+    c1.showRadius();
+    float a=calArea(c1);
+    cout<<a;
+}
