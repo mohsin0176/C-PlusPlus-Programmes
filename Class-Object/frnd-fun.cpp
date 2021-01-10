aaa810b9fcca83c7b025516272454d8eee917fc9
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 22:08:12 +0600

    class created

--- Class-Object/frnd-fun.cpp
+++ Class-Object/frnd-fun.cpp
@@ -1,1 +1,42 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class height;
+class width
+{
+    int w;
+public:
+    void getdata(int wt)
+    {
+        w=wt;
+        cout<<w;
+    }
+    friend void area(width,height);
+};
+class height
+{
 
+    int h;
+public:
+    void getdata(int ht)
+    {
+        h=ht;
+        cout<<h;
+    }
+    friend void area(width,height);
+};
+void area(width n,height m)
+{
+  int a;
+  a=n.w*m.h;
+  cout<<a;
+}
+int main()
+{
+    width n;
+    height m;
+    n.getdata(10);
+    m.getdata(20);
+    area(n,m);
+    return 0;
+}
--- Class-Object/get-set-fun.cpp
+++ Class-Object/get-set-fun.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+using namespace std;.
+class Circle
+{
+    float radius;
+public:
+    void setRadius(float r);
+    float getRadius();
+    inline float area();
+};
 
+void Circle::setRadius(float r)
+{
+    radius=r;
+}
+float Circle::getRadius()
+{
+    return radius;
+}
+inline float Circle::area()
+{
+    return PI*radius*radius;
+}
+void main()
+{
+    Circle c1;
+    c1.setRadius(10);
+    float a=c1.area();
+    cout<<c1.getRadius();
+    cout<<a;
+}
