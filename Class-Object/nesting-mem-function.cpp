2d544f43f1b1d3883cb0e22ff859512281d538c4
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 21:14:29 +0600

    class created

--- Class-Object/nesting-mem-function.cpp
+++ Class-Object/nesting-mem-function.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+    int x;
+    int y;
+public:
+    void get()
+    {
+        cin>>x>>y;
+    }
+    void showX()
+    {
+        cout<<x;
+    }
+    void showY()
+    {
+        cout<<y;
+    }
+};
+void main()
+{
+    A a1;
+    a1.get();
+    a1.showY();
+}
