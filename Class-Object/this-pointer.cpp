b2697650534cbaaa02f06aef91ac869b3ff7d257
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 20:24:35 +0600

    class created

--- Class-Object/this-pointer.cpp
+++ Class-Object/this-pointer.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+private:
+    int x;
+public:
+    void showAddress()
+    {
+        cout<<this;
+    }
+};
+void main()
+{
+    A a1,a2,a3;
+    a1.showAddress();
+    a2.showAddress();
+    a3.showAddress();
+}
