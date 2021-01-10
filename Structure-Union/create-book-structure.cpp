9a9765b39e4f4b9cf243b54f51ccd5e655e2a87c
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 18:40:22 +0600

    structure created

--- Structure-Union/create-book-structure.cpp
+++ Structure-Union/create-book-structure.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+using namespace std;
+struct book
+{
+    char name[25];
+    char author[20];
+    float price;
+};
+int  main()
+{
+    book b;
+    gets(b.name);
+    gets(b.author);
+    cin>>b.price;
+    cout<<b.name;
+    cout<<b.author;
+    cout<<b.price;
+    return 0;
+}
