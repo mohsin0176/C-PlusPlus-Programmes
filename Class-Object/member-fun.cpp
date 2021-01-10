e015f70d1c09ab0e64939e01b19dc29be181252b
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 21:27:29 +0600

    class created

--- Class-Object/member-fun.cpp
+++ Class-Object/member-fun.cpp
@@ -1,1 +1,22 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class member
+{
+public:
+    void inside()
+    {
+        cout<<"Inside function";
+    }
+    void outside();
+};
+void member::outside()
+{
+    cout<<"Outside function";
+}
+void main()
+{
+    member m;
+    m.inside();
+    m.outside();
+}
