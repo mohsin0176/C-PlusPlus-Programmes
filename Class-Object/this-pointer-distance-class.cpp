75aea205b305b5da24beba2c8fb8075908c17fb7
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 20:40:56 +0600

    class created

--- Class-Object/this-pointer-distance-class.cpp
+++ Class-Object/this-pointer-distance-class.cpp
@@ -1,1 +1,26 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    void getDist()
+    {
+        cin>>this->feet>>this->inches;
+    }
+    void showDist()
+    {
+        cout<<this->feet<<this->inches;
+    }
+};
+void main()
+{
+    Distance d1,d2;
+    d1.getDist();
+    d2.getDist();
+    d1.showDist();
+    d2.showDist();
+}
