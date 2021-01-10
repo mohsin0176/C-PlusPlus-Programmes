d2a60f27d389dd51cdec2c72445fb11df8c7884d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 18:54:44 +0600

    structure created

--- Structure-Union/pass-struc-var-to-fun.cpp
+++ Structure-Union/pass-struc-var-to-fun.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+#include<conio.h>
+struct Distance
+{
+    int feet;
+    float inches;
 
+};
+void display(Distance d)
+{
+    cout<<d.feet<<d.inches;
+}
+void main()
+{
+    Distance d1;
+    Distance d2;
+    cin>>d1.feet>>d1.inches;
+    display(d1);
+    display(d2);
+}
