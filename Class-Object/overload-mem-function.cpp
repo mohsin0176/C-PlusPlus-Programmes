73bc873c5035efab200caf505e75bb77c190e571
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 21:34:26 +0600

    class created

--- Class-Object/overload-mem-function.cpp
+++ Class-Object/overload-mem-function.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+    int x;
+public:
+    void show()
+    {
+        cout<<x;
+    }
+        void show(int x)
+    {
+        cout<<x;
+    }
+};
+int main()
+{
+    A a1;
+    a1.show();
+    a1.show(100);
+    return 0;
+}
