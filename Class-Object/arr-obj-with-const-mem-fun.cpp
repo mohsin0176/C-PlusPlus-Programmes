eb9b4148c96089e5c11b8bf1dfef11e16e876a36
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 23:00:36 +0600

    class created

--- Class-Object/arr-obj-with-const-mem-fun.cpp
+++ Class-Object/arr-obj-with-const-mem-fun.cpp
@@ -1,1 +1,32 @@
-
+#include<iostream>
+#include<conio.h>
+const int MAX=5;
+using namespace std;
+class Distance
+{
+private:
+    int feet;
+    float inches;
+public :
+    void getDist()
+    {
+        cin>>feet>>inches;
+    }
+    void showDist() const
+    {
+        cout<<feet<<inches;
+    }
+};
+int main()
+{
+    Distance d[MAX];
+    for(int i=0;i<MAX;i++)
+    {
+        d[i].getDist();
+    }
+    for(int i=0;i<MAX;i++)
+    {
+        d[i].showDist();
+    }
+    return 0;
+}
