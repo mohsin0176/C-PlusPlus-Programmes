43a889411f919e3e0d959f4d4f135c0c3badb15a
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 22:13:26 +0600

    class created

--- Class-Object/function-overload.cpp
+++ Class-Object/function-overload.cpp
@@ -1,1 +1,34 @@
 
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+using namespace std;
+const pi=3.1416;
+class shape
+{
+private:
+    float r;
+    int h,w;
+public:
+    float area(float rd)
+    {
+        r=rd;
+        return (pi*r*r);
+    }
+    int area(int ht,int wt)
+    {
+        h=ht;
+        w=wt;
+        return (h*w);
+    }
+};
+void main()
+{
+    float radius;
+    int width,height;
+    shape s;
+    cin>>radius;
+    cout<<s.area(radius);
+    cin>>height>>width;
+    cout<<s.area(height,width);
+}
