fe13aaa50c7d70853548e2f115d0b278a6650860
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 18:49:24 +0600

    structure created

--- Structure-Union/fun-overload-with-struc.cpp
+++ Structure-Union/fun-overload-with-struc.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+struct distance {
 
+int feet;
+float inches;
+};
+void display(Distance);
+void display(float);
+void main()
+{
+    Distance d1;
+    float d2;
+    cin>>d1.feet;
+    cin>>d1.inches;
+    cin>>d2;
+    display(d1);
+    display(d2);
+
+}
+void display(Distance dd)
+{
+    cout<<dd.feet<<dd.inches;
+}
+void display(float dd)
+{
+    int feet=dd/2;
+    float inches=dd-(feet*12);
+    cout<<feet<<inches;
+
+}
