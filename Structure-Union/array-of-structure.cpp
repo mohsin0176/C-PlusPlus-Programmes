b8f4e1238b41007fdcabaa4e2b2c287c862f84c6
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 18:29:52 +0600

    structure created

--- Structure-Union/array-of-structure.cpp
+++ Structure-Union/array-of-structure.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+struct Distance
+{
+    int feet;
+    float inches;
+};
+void main()
+{
+    Distance d[5];
+    int i;
+    for(i=0;i<5;i++)
+    {
+        cin>>d[i].feet>>d[i].inches;
+    }
+    for(i=0;i<5;i++)
+    {
+        cout<<d[i].feet<<d[i].inches;
+    }
+}
