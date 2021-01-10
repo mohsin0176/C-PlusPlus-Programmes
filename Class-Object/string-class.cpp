c406c08b694d8980af905ca80b358a4d81d97314
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 20:20:59 +0600

    class created

--- Class-Object/string-class.cpp
+++ Class-Object/string-class.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+class String
+{
+    char str[80];
+public:
+    String()
+    {
+        str[0]='\0';
 
+    }
+    String (char s[])
+    {
+        strcpy(str,s);
+    }
+    void display()
+    {
+        cout<<str;
+    }
+};
+void main()
+{
+    String s1("c++");
+    String s2="Programming";
+    String s3("Examples");
+    s1.display();
+    s2.display();
+    s3.display();
+
+}
