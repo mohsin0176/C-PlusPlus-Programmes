9fce9fcae7786fe94849b8b443275fb287ec7c6f
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 22:28:31 +0600

    class created

--- Class-Object/frnd-fun-to-two-classes.cpp
+++ Class-Object/frnd-fun-to-two-classes.cpp
@@ -1,1 +1,45 @@
-
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+using namespace std;
+class abc;
+class xyz
+{
+    int x;
+public:
+    void setValue(int i)
+    {
+        x=i;
+    }
+    friend void max(xyz,abc);
+};
+class abc
+{
+    int a;
+public:
+    void setvalue(int i)
+    {
+        a=i;
+    }
+    friend void max(xyz,abc);
+};
+void max(xyz m,abc n)
+{
+    if(m.x>=n.a)
+    {
+        cout<<m.x;
+    }
+    else
+    {
+        cout<<n.a;
+    }
+}
+int main()
+{
+    xyz kmp;
+    kmp.setValue(10);
+    abc dmp;
+    dmp.setvalue(20);
+    max(kmp,dmp);
+    return 0;
+}
