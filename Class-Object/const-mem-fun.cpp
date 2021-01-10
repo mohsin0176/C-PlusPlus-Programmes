d04c16a162c63e141b3802314fb70b55b1c6b6c1
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 22:33:32 +0600

    class created

--- Class-Object/const-mem-fun.cpp
+++ Class-Object/const-mem-fun.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+    int x;
+public:
+    void getX()
+    {
+        cin>>x;
+    }
+    void showX() const
+    {
+        cout<<x;
+    }
+};
+int main()
+{
+    A a1;
+    a1.getX();
+    a1.showX();
+    return 0;
+}
