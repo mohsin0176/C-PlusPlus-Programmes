89cd93d3805bd4f971bd236f9499817ebf3e2e1d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 19:48:55 +0600

    class created

--- Class-Object/simple-class-object.cpp
+++ Class-Object/simple-class-object.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+#include<conio.h>
+class Demo
+{
+public:
+    void display()
+    {
+        cout<<"Welcome To C++";
+    }
+};
+void main()
+{
+    Demo d;
+    d.display();
+}
