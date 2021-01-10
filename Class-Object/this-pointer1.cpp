1045a66dcac5efe1309691e28c9258754edcecd5
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 20:33:43 +0600

    class created

--- Class-Object/this-pointer1.cpp
+++ Class-Object/this-pointer1.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+class Circle
+{
+    float radius;
+public:
+    void setRadius(float radius);
+    float getRadius();
+    float area();
 
+};
+void Circle::setRadius(float radius)
+{
+    this->radius=radius;
+}
+float Circle::getRadius()
+{
+    this->radius=radius;
+}
+float Circle::area()
+{
+    return PI*radius*radius;
+}
+int main()
+{
+    Circle c1;
+    c1.setRadius(10);
+    float a=c1.area();
+    cout<<c1.getRadius();
+    cout<<a;
+    return 0;
+}
