2237af6af0c61609e20d89893a22dd289b1efb68
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 19:58:24 +0600

    class created

--- Class-Object/simple-distance-class.cpp
+++ Class-Object/simple-distance-class.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Distance
+{
+    private:
+        int feet;
+        float inches;
+    public:
+        void getDist()
+        {
+            cin>>feet;
+            cin>>inches;
+        }
+        void showDist()
+        {
+            cout<<feet<<inches;
+        }
 
+};
+int main()
+{
+    Distance d1,d2;
+    d1.getDist();
+    d2.getDist();
+    d1.showDist();
+    d2.showDist();
+    return 0;
+}
