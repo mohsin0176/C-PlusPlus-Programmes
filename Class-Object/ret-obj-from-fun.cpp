2e89668803a1556edcf0fb24657aa96c2ddd9da7
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 20:51:05 +0600

    class created

--- Class-Object/ret-obj-from-fun.cpp
+++ Class-Object/ret-obj-from-fun.cpp
@@ -1,1 +1,42 @@
-
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+class Circle
+{
+    float radius;
+public:
+    void setRadius();
+    float getRadius();
+    void showRadius();
+};
+void Circle::setRadius()
+{
+    cin>>radius;
+}
+void Circle::getRadius()
+{
+    return radius;
+}
+Circle createCircle()
+{
+    Circle c;
+    c.setRadius();
+    return c;
+}
+float calArea(Circle c)
+{
+    float ar;
+    ar=PI*c.getRadius()*c.getRadius();
+    return ar;
+}
+void Circle:showRadius()
+{
+    cout<<radius;
+}
+void main()
+{
+    Circle c1=createCircle();
+    c1.showRadius();
+    float a=calArea(c1);
+    cout<<a;
+}
