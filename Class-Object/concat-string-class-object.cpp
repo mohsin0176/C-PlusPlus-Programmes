108331240656321203a1f93c8e2f9db6e869bd73
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 22:43:31 +0600

    class created

--- Class-Object/concat-string-class-object.cpp
+++ Class-Object/concat-string-class-object.cpp
@@ -1,1 +1,55 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+const int SZ=80;
+class String
+{
+    char str[SZ];
+public:
+    String()
+    {
+        strcpy(str,"\0");
+    }
+    String(char s[ ])
+    {
+        strcpy(str,s);
+    }
+    void display()
+    {
+        cout<<str;
+    }
+    String concat(String);
+};
+String String ::concat(String S2)
+{
+    String tmp;
+    if((strlen(str)+strlen(s2.str))<SZ)
+    {
+        strcpy(tmp.str,str);
+        strcat(tmp.str,s2.str);
+    }
+    else
+    {
+        cout<<"String Long";
+    }
+    return tmp;
+}
+void main()
+{
+    String s1("c++");
+    String s2="Programming";
+    String s3("Examples");
+    String s4;
+    s1.display();
+    s2.display();
+    s3.display();
+    s4=s1;
+    s4=s1.concat(s2);
+    s4.display();
+    s4.display();
+    s4=s4.concat(s2);
+    s4.display();
+    s4=s4.concat(s3);
+    s4.display();
 
+}
