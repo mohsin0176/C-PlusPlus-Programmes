96e28d6c0c93db9ca375bfefb02fdc64acee7e29
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 19:53:02 +0600

    class created

--- Class-Object/simple-class-object2.cpp
+++ Class-Object/simple-class-object2.cpp
@@ -1,1 +1,30 @@
-
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+class Circle
+{
+private:
+    float radius;
+public:
+    void getRadius()
+    {
+        cin>>radius;
+    }
+    void area()
+    {
+        float ar;
+        ar=PI*radius*radius;
+        cout<<ar;
+    }
+    void showRadius()
+    {
+        cout<<radius;
+    }
+};
+void main()
+{
+    Circle c1;
+    c1.getRadius();
+    c1.showRadius();
+    c1.area();
+}
