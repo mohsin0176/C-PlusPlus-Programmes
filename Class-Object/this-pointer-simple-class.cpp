d3faaae30b073e5b570329ba95279a15f4fe7c8c
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-10 20:37:36 +0600

    class created

--- Class-Object/this-pointer-simple-class.cpp
+++ Class-Object/this-pointer-simple-class.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+private:
+    int x;
+public:
+    void getX()
+    {
+        cin>>this->x;
+    }
+    void callgetX()
+    {
+        cin>>this->getX();
+    }
+    int showX()
+    {
+        return this->x;
+    }
+};
+void main()
+{
+    A a1;
+    a1.callgetX();
+    cout<<a1.showX();
+}
