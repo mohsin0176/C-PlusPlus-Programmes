1ace371348aefb56ec6ee8af485d66fc656072c9
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 14:16:57 +0600

    constructor  created

--- Constructor-Destructor/default-construc.cpp
+++ Constructor-Destructor/default-construc.cpp
@@ -1,1 +1,38 @@
-
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+using namespace std;
+class Circle{
+float radius;
+public:
+    Circle();
+    void getRadius();
+    float area();
+    void showRadius();
+};
+Circle::Circle()
+{
+    radius=10;
+}
+void Circle::getRadius()
+{
+    cin>>radius;
+}
+float Circle::area()
+{
+    float ar;
+    ar=PI*radius*radius;
+    return ar;
+}
+void Circle::showRadius()
+{
+    cout<<radius;
+}
+int  main()
+{
+    Circle c1;
+    c1.showRadius();
+    float a=c1.area();
+    cout<<a;
+    return 0;
+}
