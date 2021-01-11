0a5a4c1eb10df961f7818d5c9d2b619c8435172e
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 20:11:06 +0600

    operator  created

--- Operator-Overloading/overload insertion-extraction opeartor.cpp
+++ Operator-Overloading/overload insertion-extraction opeartor.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    Distance()
+    {
+        feet=0;
+        inches=0;
+    }
+    friend istream& operator>>(istream&,Distance&);
+    friend ostream& operator>>(ostream&,Distance&);
 
+};
+istream& operator>>(istream &in,Distance &d)
+{
+    in>>d.feet>>d.inches;
+}
+ostream& operator<<(ostream &out,Distance &d)
+{
+    out<<d.feet<<d.inches;
+}
+int main()
+{
+    Distance d1;
+    Distance d2;
+    cin>>d1>>d2;
+    cout<<d1<<d2;
+    return 0;
+}
