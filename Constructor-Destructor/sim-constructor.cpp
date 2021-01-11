c2d4e06a175ec5237441a60067b470a53c5c0a06
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 13:48:53 +0600

    constructor  created

--- Constructor-Destructor/sim-constructor.cpp
+++ Constructor-Destructor/sim-constructor.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Demo
+{
+public:
+    demo()
+    {
+        cout<<"constructor"<<endl;
+        cout<<"called without object"<<endl;
+    }
+};
+int main()
+{
+    Demo d;
+return 0;
+}
