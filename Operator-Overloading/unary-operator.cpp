c913d2b81a4ae1a766aaf8c7141983a4b9829fe0
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 18:47:47 +0600

    operator  created

--- Operator-Overloading/unary-operator.cpp
+++ Operator-Overloading/unary-operator.cpp
@@ -1,1 +1,34 @@
-
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+using namespace std;
+class increase
+{
+private:
+    int data;
+public:
+    increase()
+    {
+        data=0;
+    }
+    int display()
+    {
+        cout<<data<<endl;
+    }
+    void operator++()
+    {
+        data++;
+    }
+};
+int main()
+{
+    increase i1,i2;
+    i1.display();
+    i2.display();
+    i1++;
+    ++i1;
+    i2++;
+    i1.display();
+    i2.display();
+    return 0;
+}
