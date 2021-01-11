e34d35305d6602f30c56cb025bfd1e3bd8528708
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 15:47:54 +0600

    constructor  created

--- Constructor-Destructor/constructor-with-default-arg.cpp
+++ Constructor-Destructor/constructor-with-default-arg.cpp
@@ -1,1 +1,24 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+    int x;
+public:
+    A(int a=10)
+    {
+        x=a;
+    }
+    void showX()
+    {
+        cout<<x;
+    }
+};
+int main()
+{
+    A a1;
+    A a2(100);
+    a1.showX();
+    a2.showX();
+    return 0;
+}
