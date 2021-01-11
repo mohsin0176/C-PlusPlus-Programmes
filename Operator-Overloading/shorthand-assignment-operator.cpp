fca6447965d22dc5079d1d8c3d73c6450cdc4e5c
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 19:40:28 +0600

    operator  created

--- Operator-Overloading/shorthand-assignment-operator.cpp
+++ Operator-Overloading/shorthand-assignment-operator.cpp
@@ -1,1 +1,40 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class height
+{
+private:
+    int feet;
+    float inch;
+public:
+    height()
+    {
+        feet=inches=0;
+    }
+    height(int f,float i)
+    {
+        feet=f;
+        inches=i;
+    }
+    void show()
+    {
+        cout<<feet<<inches;
+    }
+    void operator+=(height h)
+    {
+        feet+=h.feet;
+        inches+=h.inches;
+        if(inches>=12)
+        {
+            inches=12;
+            feet++;
+        }
+    }
+};
+int main()
+{
+    height h1(10,15),h2(7,18);
+    h1+=h2;
+    h1.show();
+    return 0;
+}
--- Operator-Overloading/shorthand-hand-assignment-operator.cpp
+++ Operator-Overloading/shorthand-hand-assignment-operator.cpp
@@ -1,1 +1,47 @@
-
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+const int SZ=80;
+class String
+{
+    char str[SZ];
+public:
+    String()
+    {
+        strcpy(str,'\0');
+    }
+    String (char s[ ])
+    {
+        strcpy(str,s);
+    }
+    void display()
+    {
+        cout<<str;
+    }
+    void operator+=(String);
+};
+void String::operator+=(String s2)
+{
+    if((strlen(str)+strlen(s2.str))<SZ)
+    {
+        strcat(str,s2.str);
+    }
+    else
+    {
+        cout<<"String Too Long";
+    }
+}
+int main()
+{
+    String s1("C++");
+    String s2="Programming";
+    String s3("Example");
+    s1.display();
+    s2.display();
+    s3.display();
+    s1+=s2;
+    s1.display();
+    s1+=s3;
+    s1.display();
+    return 0;
+}
