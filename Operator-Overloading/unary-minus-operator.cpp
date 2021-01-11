d2558ed50cc0b7cfa6ee30ad97faab1d47811045
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 18:54:20 +0600

    operator  created

--- Operator-Overloading/unary-minus-operator.cpp
+++ Operator-Overloading/unary-minus-operator.cpp
@@ -1,1 +1,34 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class counter
+{
+unsigned int count;
+public:
+    Counter()
+    {
+        count=0;
+    }
+    Counter (int c)
+    {
+        count=c;
+    }
+    int getCount()
+    {
+        return count;
+    }
+    void operator-()
+    {
+        count=-count;
+    }
+};
+int main()
+{
+    Counter c1(10),c2(20);
+    cout<<c1.getCount();
+    cout<<c2.getCount();
+    -c2;
+    cout<<c1.getCount();
+    cout<<c2.getCount();
+    return 0;
+}
