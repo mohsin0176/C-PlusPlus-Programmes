080a79d78648e838fb4b8421fd564a75481178a8
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 14:22:28 +0600

    constructor  created

--- Constructor-Destructor/parameterized-construct.cpp
+++ Constructor-Destructor/parameterized-construct.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+#include<conio.h>
+class Volume
+{
+    int length,breadth,height;
+public:
+    volume(int l,int b,int h)
+    {
+        length=l;
+        breadth=b;
+        height=h;
+        cout<<length;
+        cout<<breadth;
+        cout<<height;
+    }
+    void display()
+    {
+        cout<<length*breadth*height;
+    }
+};
+void main()
+{
+    volume a(5,10,15);
+    a.display();
 
+}
