682f9178a53393b5449b771fde207fa9b17466ca
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 15:40:14 +0600

    constructor  created

--- Constructor-Destructor/default-copy-constructor-by-compiler.cpp
+++ Constructor-Destructor/default-copy-constructor-by-compiler.cpp
@@ -1,1 +1,28 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+private:
+    int x;
+public:
+    A(){};
+    A(int a){x=a;}
+    void showX()
+    {
+        cout<<x;
+    }
+};
+int main()
+{
+    A a1(10);
+    A a2;
+    a1.showX();
+    a2=a1;
+    a2.showX();
+    A a3=a1;
+    a3.showX();
+    A a4(a1);
+    a4.showX();
+    return 0;
+}
