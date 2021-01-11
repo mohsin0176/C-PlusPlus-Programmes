90d09911b8c62da4a87315283193fb6b577d24df
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 17:00:03 +0600

    constructor  created

--- Constructor-Destructor/destructor.cpp
+++ Constructor-Destructor/destructor.cpp
@@ -1,1 +1,35 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class myclass
+{
+private:
+    int t;
+public:
+    myclass()
+    {
+        cout<<"constructor";
+    }
+    ~myclass()
+    {
+        cout<<"destructor";
+    }
+    void set (int d)
+    {
+        t=d;
+    }
+    void show()
+    {
+        cout<<t<<endl;
+    }
+};
+int main()
+{
+    myclass m1,m2;
+    m1.set(2);
+    m2.set(6);
+    m1.show();
+    m2.show();
+    cout<<"latest statement";
+    return 0;
+}
--- Constructor-Destructor/destructor1.cpp
+++ Constructor-Destructor/destructor1.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int count=0;
+class dest{
+public:
+    dest()
+         {
+             count++;
+             cout<<"constructor"<<endl;
+         }
+     ~dest()
+     {
+         count--;
+             cout<<"destructor"<<endl;
+     }
 
+
+         };
+
+int main()
+{
+
+    dest d1,d2,d3,d4;
+    {
+        dest d5,d6;
+    }
+    {
+        dest d7;
+    }
+    return 0;
+}
