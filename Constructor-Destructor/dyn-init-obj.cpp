fa390334b3d03c87e7867a40e3374d2c2ac3b132
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 15:43:48 +0600

    constructor  created

--- Constructor-Destructor/dyn-init-obj.cpp
+++ Constructor-Destructor/dyn-init-obj.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+    int x;
+public:
+    A(int a)
+    {
+        x=a;
+    }
+    void showX()
+    {
+        cout<<x;
+    }
+};
+int main()
+{
+    int a;
+    cin>>a;
+    A a1(a);
+    a1.showX();
 
+    return 0;
+}
