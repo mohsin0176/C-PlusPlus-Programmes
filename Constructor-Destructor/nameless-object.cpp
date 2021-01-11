13974a2f9f97cb0a4e4a68c81fd07a19f465e7a2
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 16:00:18 +0600

    constructor  created

--- Constructor-Destructor/nameless-object.cpp
+++ Constructor-Destructor/nameless-object.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+    int x;
+public:
+    A(int a=10)
+    {
+        x=a;
+    }
+    void showX()
+    {
+        cout<<x;
+    }
 
+};
+int main()
+{
+    A().showX();
+    A(100).showX();
+    return 0;
+}
