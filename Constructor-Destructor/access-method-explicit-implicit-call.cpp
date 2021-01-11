07245c0a32d92d62c5d5c06eda17fda5e93291f7
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 16:05:38 +0600

    constructor  created

--- Constructor-Destructor/access-method-explicit-implicit-call.cpp
+++ Constructor-Destructor/access-method-explicit-implicit-call.cpp
@@ -1,1 +1,28 @@
-
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+using namespace std;
+class ObjectMethod
+{
+    int a,b;
+public:
+    ObjectMethod(int,int);
+    void display(void)
+    {
+        cout<<a;
+        cout<<b;
+    }
+};
+ObjectMethod::ObjectMethod(int x,int y)
+{
+    a=x;
+    b=y;
+}
+int main()
+{
+    ObjectMethod obj1=ObjectMethod(23,95);
+    ObjectMethod obj2(41,77);
+    obj1.display();
+    obj2.display();
+    return 0;
+}
