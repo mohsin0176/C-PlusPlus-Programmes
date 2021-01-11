158adca79810094c4c84e38dc419a6eddfce1193
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 17:20:16 +0600

    constructor  created

--- Constructor-Destructor/calling-of-member-function-with-functional-notation.cpp
+++ Constructor-Destructor/calling-of-member-function-with-functional-notation.cpp
@@ -1,1 +1,55 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    Distance()
+    {
+        feet=0;
+        inches=0;
 
+    }
+    Distance(int ft,float in)
+    {
+        feet=ft;
+        inches=in;
+
+    }
+    Distance(float f)
+    {
+        feet=int(f);
+        inches=12*(f-feet);
+
+    }
+    void getDist()
+    {
+        cin>>feet>>inches;
+    }
+    void showDist()
+    {
+         cout<<feet<<endl<<inches<<endl;
+    }
+    Distance square();
+};
+Distance Distance::square()
+{
+    Distance dd;
+    float ft=feet+inches/12;
+    ft=ft*ft;
+    dd.feet=int(ft);
+    dd.inches=12*(ft-dd.feet);
+    return dd;
+}
+int main()
+{
+    Distance d1(12,9);
+    Distance d2;
+    d1.showDist();
+    d2=d1.square();
+    d2.showDist();
+    return 0;
+}
