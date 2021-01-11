ac00e002f61c4e0189a7b5f5211fe027f68dc894
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 16:24:14 +0600

    constructor  created

--- Constructor-Destructor/explain-use-copy-constructor.cpp
+++ Constructor-Destructor/explain-use-copy-constructor.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    Distance()
+    {
+        feet=0;
+        inches=0;
+    }
+    Distance(int ft,float in)
+    {
+        feet=ft;
+        inches=in;
+    }
+    Distance(Distance &d)
+    {
+        feet=d.feet;
+        inches=d.inches;
+    }
+    void showDist()
+    {
+        cout<<feet<<inches;
+    }
+};
+int main()
+{
+    Distance d1(10,20);
+    Distance d2(d1);
+    Distance d3=d1;
+    d1.showDist();
+    d2.showDist();
+    d3.showDist();
+    return 0;
 
+    return 0;
+}
