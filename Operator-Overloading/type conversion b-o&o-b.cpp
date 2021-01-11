3db715382c3b22cd0da929f7a7e72ad8d65aa9f9
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 20:01:59 +0600

    operator  created

--- Operator-Overloading/type conversion b-o&o-b.cpp
+++ Operator-Overloading/type conversion b-o&o-b.cpp
@@ -1,1 +1,55 @@
+#include<iostream>
+#include<conio.h>
+const float CF=3.280833;
+using namespace std;
+class Distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    Distance()
+    {
+        feet=0;
+        inches=0;
+    }
+    Distance(float m)
+    {
+        float ft=m*CF;
+        feet=(int)ft;
+        inches=12*(ft-feet);
+    }
+    Distance(int ft,float in)
+    {
+        feet=ft;
+        inches=in;
+    }
+    void getDist()
+    {
+        cin>>feet>>inches;
+    }
+    void showDist()
+    {
+        cout<<feet<<inches;
+    }
+    operator float()
+    {
+        float ft=feet+inches/12;
+        return (ft/CF);
+    }
+};
 
+
+int main()
+{
+    Distance d1=1;
+    d1.showDist();
+    d1=2;
+    d1.showDist();
+    Distance d2(10,60);
+    float m=float(d2);
+    cout<<m;
+    m=d1;
+    cout<<m;
+    return 0;
+}
--- Operator-Overloading/type conversion b-o&o-b1.cpp
+++ Operator-Overloading/type conversion b-o&o-b1.cpp
@@ -1,1 +1,38 @@
-
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+const int SZ=80;
+class String
+{
+    char str[SZ];
+public:
+    String()
+    {
+        strcpy(str,"\0");
+    }
+    String(char s[ ])
+    {
+        strcpy(str,s);
+    }
+    void display()
+    {
+        cout<<str;
+    }
+    operator char*()
+    {
+        return str;
+    }
+};
+int main()
+{
+    String s1;
+    char st[]="c++";
+    s1=st;
+    String s2="Programming";
+    String s3("Examples");
+    cout<<(char*)s1;
+    cout<<(char*)s2;
+    cout<<(char*)s3;
+    return 0;
+}
