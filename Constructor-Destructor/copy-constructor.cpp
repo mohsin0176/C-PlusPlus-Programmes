dae3b9c139d7355a901ffe098b2b43e9f981c602
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 15:09:09 +0600

    constructor  created

--- Constructor-Destructor/copy-constructor.cpp
+++ Constructor-Destructor/copy-constructor.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class code{
 
+int id;
+public:
+    code(){}
+    code(int a)
+    {
+        id=a;
+    }
+    code(code &x)
+    {
+        id=x.id;
+    }
+    void display(void)
+    {
+        cout<<id;
+    }
+};
+int main()
+{
+    code A(100);
+    code B(A);
+    code C=A;
+    code D;
+    D=A;
+    cout<<A.display();
+    cout<<B.display();
+    cout<<C.display();
+    cout<<D.display();
+
+
+
+    return 0;
+}
