6803fa9c2226d18d7919a4a8b361bdfea3fafa82
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 22:30:46 +0600

    inheritance  created

--- Inheritance/private visibility mode.cpp
+++ Inheritance/private visibility mode.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
 
+{
+    int x;
+public:
+    void getX()
+    {
+        cin>>x;
+    }
+    void showX()
+    {
+        cout<<x;
+    }
+};
+class B:private A
+{
+    int y;
+public:
+    void getY()
+    {
+        cin>>y;
+    }
+    void showY()
+    {
+        cout<<y;
+    }
+};
+int main()
+{
+    B b1;
+    b1.getY();
+    b1.showY();
+    return 0;
+}
