209787f2a6b9664f2b160fea922d9f3fa27403f5
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 16:18:21 +0600

    constructor  created

--- Constructor-Destructor/diff-tech-init-data-mem-construc.cpp
+++ Constructor-Destructor/diff-tech-init-data-mem-construc.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    Distance():feet(0),inches(0){}
+    Distance(int ft,float in):feet(ft),inches(in){}
+    void getDist()
+    {
+        cin>>feet>>inches;
+    }
+    void showDist()
+    {
+        cout<<feet<<inches;
+    }
+};
 
+int main()
+{
+    Distance d1,d2;
+    d2.getDist();
+    Distance d3(10,60);
+    d1.showDist();
+    d2.showDist();
+    d3.showDist();
+    return 0;
+}
