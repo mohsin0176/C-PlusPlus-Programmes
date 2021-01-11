df0b12624b9b48ff4e60ba160039661b6660588d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 16:33:05 +0600

    constructor  created

--- Constructor-Destructor/default-construc-counter1.cpp
+++ Constructor-Destructor/default-construc-counter1.cpp
@@ -1,1 +1,30 @@
 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Counter
+{
+    unsigned int count;
+public:
+    Counter():count(0){}
+    void inc_count()
+    {
+        count++;
+    }
+    int getCount()
+    {
+        return count;
+    }
+};
+int main()
+{
+    Counter c1,c2;
+    cout<<c1.getCount();
+    cout<<c2.getCount();
+    c1.inc_count();
+    c2.inc_count();
+    c2.inc_count();
+    cout<<c1.getCount();
+    cout<<c2.getCount();
+    return 0;
+}
