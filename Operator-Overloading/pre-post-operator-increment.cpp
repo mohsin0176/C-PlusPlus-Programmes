1742fe8964f727e0d318d9c515f211635edf5dbd
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 19:02:10 +0600

    operator  created

--- Operator-Overloading/pre-post-operator-increment.cpp
+++ Operator-Overloading/pre-post-operator-increment.cpp
@@ -1,1 +1,38 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Counter
+{
+    unsigned int count;
+public:
+    Counter()
+    {
+        count=0;
+    }
+    int getCount()
+    {
+        return count;
+    }
+    void operator++()
+    {
+        count++;
+        cout<<"Prefix";
+    }
+    void operator ++(int)
+    {
+        count++;
+        cout<<"Postfix";
+    }
+};
+int main()
+{
+    Counter c1,c2;
+    cout<<c1.getCount();
+    cout<<c2.getCount();
+    ++c1;
+    ++c2;
+    c2++;
+    cout<<c1.getCount();
+    cout<<c2.getCount();
+    return 0;
+}
