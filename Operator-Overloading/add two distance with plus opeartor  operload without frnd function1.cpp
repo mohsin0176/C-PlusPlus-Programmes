6e0b74ca08d1783d5b16736baeac5215e09a1fd3
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 20:54:49 +0600

    operator  created

--- Operator-Overloading/add two distance with plus opeartor  operload without frnd function1.cpp
+++ Operator-Overloading/add two distance with plus opeartor  operload without frnd function1.cpp
@@ -1,1 +1,62 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    Distance()
+    {
+        feet=0;
+        inches=0;
+    }
+    Distance(int ft,float in)
+    {
+        feet=ft;
+        inches=in;
+    }
+    Distance(float f)
+    {
+        feet=int(f);
+        inches=12*(f-feet);
+    }
+    void getDist()
+    {
+        cin>>feet>>inches;
+    }
+    void showDist()
+    {
+        cout<<feet<<inches;
+    }
+    friend Distance operator + (Distance,Distance);
+};
+ Distance operator + (Distance dd1 ,Distance dd2)
+{
+    Distance dd3;
+    dd3.feet=feet+dd2.feet;
+    dd3.inches=inches+dd2.inches;
+    if(dd3.inches>=12)
+    {
+        dd3.inches-=12;
+        dd3.feet++;
+
+    }
+    return dd3;
+}
+
+int main()
+{
+    Distance d1(12,9);
+    Distance d2(10,6);
+    Distanced3,d4,d5;
+    d3=d1+d2;
+    d3.showDist();
+    d4=d1+10;
+    d4.showDist();
+    d5=10+d1;
+    d5.showDist();
+    return 0;
+}
 
