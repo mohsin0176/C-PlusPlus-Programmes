8074dfe466a54b8823e3c02e96a217df3a1ffd74
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 16:11:52 +0600

    constructor  created

--- Constructor-Destructor/explicit-construc-call.cpp
+++ Constructor-Destructor/explicit-construc-call.cpp
@@ -1,1 +1,37 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
 
+class Distance()
+{
+ private :
+     int feet;
+     float inches;
+ public:
+    Distance()
+    {
+        feet=0;
+        inches=0;
+
+    }
+    Distance(int ft,float in)
+    {
+        feet=ft;
+        inches=in;
+    }
+    void showDist()
+    {
+        cout<<feet<<inches;
+    }
+};
+int main()
+{
+    Distance d1=Distance();
+    Distance d2=Distance();
+    Distance d3=Distance(10,60);
+    d1.showDist();
+    d2.showDist();
+    d3.showDist()
+
+    return 0;
+}
