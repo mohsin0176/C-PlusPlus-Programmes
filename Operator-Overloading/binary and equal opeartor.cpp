f654055e8e467da8eb2214e804778f646c032885
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 20:32:55 +0600

    operator  created

--- Operator-Overloading/binary and equal opeartor.cpp
+++ Operator-Overloading/binary and equal opeartor.cpp
@@ -1,1 +1,57 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class data
+{
+private:
+    int d;
+public:
+    data()
+    {
+        d=0;
+    }
+    data(int dt)
+    {
+        dt=0;
+    }
+    int operator>(data obj)
+    {
+        if(d>obj.d)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
 
+    }
+    int operator==(data obj)
+    {
+        if(d==obj.d)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
+int main()
+{
+    data t1(43),t2(20);
+    if(t1==t2)
+    {
+        cout<<"Numbers are Equal"
+    }
+    else if(t1>t2)
+    {
+        cout<<"T1 Greater";
+    }
+    else
+    {
+        cout<<"T2 Greater";
+    }
+    return 0;
+}
