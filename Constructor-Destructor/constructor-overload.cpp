78320c5573e4c6ba65fd5b5d8b92d3e0611a0b82
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-11 14:38:39 +0600

    constructor  created

--- Constructor-Destructor/constructor-overload.cpp
+++ Constructor-Destructor/constructor-overload.cpp
@@ -1,1 +1,46 @@
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+using namespace std;
+class Circle
+{
+    float radius;
+public:
+    Circle();
+    Circle(float);
+    void getRadius();
+    float area();
+    void showRadius();
+};
 
+Circle::Circle()
+{
+    radius=10;
+}
+Circle::Circle(float r )
+{
+    radius=r;
+}
+void Circle::getRadius()
+{
+    cin>>radius;
+}
+float Circle::area()
+{
+    float ar;
+    ar=PI*radius*radius;
+    return ar;
+}
+void Circle::showRadius()
+{
+    cout<<radius;
+}
+int main()
+{
+    Circle c1(10);
+    c1.showRadius();
+    float a=c1.area();
+    cout<<a;
+    return 0;
+
+}
