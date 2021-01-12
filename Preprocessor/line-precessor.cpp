e8493d017a8d7b2f591f3facd3b6dce8162ca090
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 21:50:01 +0600

    preprocessor created

--- Preprocessor/line-precessor.cpp
+++ Preprocessor/line-precessor.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    #line 100
+    cout<<__FILE__;
+    cout<<__LINE__;
+    #line 200
+      cout<<__FILE__;
+    cout<<__LINE__;
 
+    return 0;
+}
