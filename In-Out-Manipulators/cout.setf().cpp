f4edb22bd5dafbbb1a0567c99d80e4bf6cef2037
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 22:43:23 +0600

    I/O created

--- In-Out-Manipulators/cout.setf().cpp
+++ In-Out-Manipulators/cout.setf().cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+#include<conio.h>
+#include<iomanip.h>
+using namespace std;
+int main()
+{
+    cout.setf(ios::left,ios::adjustfield);
+    cout.fill('*');
+    cout.precision(2);
+    cout.width(8);
+    cout<<12.34569;
+    cout.width(8);
+    cout<<12.3;
+    cout<<width(8);
+    cout<<12;
+    return 0;
+}
--- In-Out-Manipulators/cout.width().cpp
+++ In-Out-Manipulators/cout.width().cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout.width();
+    cout<<22;
+    cout.width(8);
+    cout<<4444;
+    cout.width(8);
+    cout<<666666;
+    return 0;
+}
