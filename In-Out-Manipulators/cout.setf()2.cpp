c4f93cfa8ac015d264a4cd62526dbd7259b07c9e
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 22:48:58 +0600

    I/O created

--- In-Out-Manipulators/cout.setf()2.cpp
+++ In-Out-Manipulators/cout.setf()2.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip.h>
+using namespace std;
+int main()
+{
+    cout.setf(ios::showpos);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    cout.width(8);
+    cout<<12.3456;
+    cout.width(8);
+    cout<<-12.3456;
+    cout.width(8);
+    cout<<12;
 
+    return 0;
+}
