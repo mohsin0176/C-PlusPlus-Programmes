d219a75fe98ddd326efdd6a89e889a022a730d8f
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 23:04:53 +0600

    I/O created

--- In-Out-Manipulators/setiosflags()1.cpp
+++ In-Out-Manipulators/setiosflags()1.cpp
@@ -1,1 +1,16 @@
 
+#include<iostream>
+#include<conio.h>
+#include<iomanip.h>
+using namespace std;
+int main()
+{
+    cout<<setiosflags(ios::showpos);
+    cout<<setiosflags(ios::showpoint);
+    cout<<setiosflags(ios::scientific);
+    cout<<precision(2);
+    cout<<setw(8)<<12.345678;
+     cout<<setw(8)<<-12.345678;
+      cout<<setw(8)<<12.34000;
+    return 0;
+}
--- In-Out-Manipulators/setiosflags()2.cpp
+++ In-Out-Manipulators/setiosflags()2.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+#include<conio.h>
+#include<iomanip.h>
+using namespace std;
+int main()
+{
+    cout<<setiosflags(ios::showpos);
+    cout<<setiosflags(ios::showpoint);
+    cout<<setiosflags(ios::scientific);
+    cout<<setiosflags(ios::fixed|ios::left);
+    cout<<precision(2);
+    cout<<setfill('*');
+    cout<<setw(8)<<12.345678;
+     cout<<setw(8)<<-12.345678;
+      cout<<setw(8)<<12.34000;
+    return 0;
+}
