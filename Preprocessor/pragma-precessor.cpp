833730cca959baac8fa2e09d381f46659eb3d2f3
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 21:48:23 +0600

    preprocessor created

--- Preprocessor/pragma-precessor.cpp
+++ Preprocessor/pragma-precessor.cpp
@@ -1,1 +1,19 @@
-
+#include<iostream>
+#include<conio.h>
+void fun1();
+void fun2();
+#pragma startup fun1
+#pragma exit fun2
+int main()
+{
+    cout<<"Inside Main";
+    return 0;
+}
+void fun1()
+{
+    cout<<"function1";
+}
+void fun2()
+{
+    cout<<"function2";
+}
