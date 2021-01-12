ed5610f0d7c5ae8a790e5dc0bbb536d481a0a8d7
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 22:56:02 +0600

    I/O created

--- In-Out-Manipulators/setbase().cpp
+++ In-Out-Manipulators/setbase().cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include<conio.h>
+#include<iomanip.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>setbase(10)>>n;
+    cout<<setbase(10)<<n;
+     cout<<setbase(8)<<n;
+      cout<<setbase(16)<<n;
+      return 0;
+}
