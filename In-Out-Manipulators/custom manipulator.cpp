a282a9dc2e4e4c94c40d9b9171ae77e70055df32
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 22:53:50 +0600

    I/O created

--- In-Out-Manipulators/custom manipulator.cpp
+++ In-Out-Manipulators/custom manipulator.cpp
@@ -1,1 +1,22 @@
-
+#include<iostream>
+#include<conio.h>
+#include<iomanip.h>
+using namespace std;
+ostream& rupees(ostream &out)
+{
+    out<<flush;
+    return out;
+}
+ostream& dollar(ostream &out)
+{
+    out<<flush;
+    return out;
+}
+int main()
+{
+    float prc;
+    cin>>prc;
+    cout<<rupees<<prc;
+    cout<<dollar<<prc;
+    return 0;
+}
