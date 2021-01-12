b0b6a02786c5204834cdc020bf1015b8b40878bd
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 22:57:47 +0600

    I/O created

--- In-Out-Manipulators/setw().cpp
+++ In-Out-Manipulators/setw().cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+#include<conio.h>
+#include<iomanip.h>
+using namespace std;
+int main()
+{
+    cout<<setw(8)<<22;
+    cout<<setw(8)<<4444;
+    cout<<setw(8)<<666666;
+    return 0;
+}
