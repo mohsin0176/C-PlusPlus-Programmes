126a5ff8074940a388076c7f786dca1f4af90d70
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 21:59:54 +0600

    preprocessor created

--- Preprocessor/compiler-control-directives2.cpp
+++ Preprocessor/compiler-control-directives2.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+#include<conio.h>
+#define INDIA 1
+#define USA 1
+#ifdef (defined INDIA)
+#define PI 3.1416
+#endif
+#ifdef (defined USA)
+#define PI 3.14
+#endif
+int main()
+     {
+         float r,a;
+         cin>>r;
+         a=PI*r*r;
+         cout<<a;
+         return 0;
+     }
 
