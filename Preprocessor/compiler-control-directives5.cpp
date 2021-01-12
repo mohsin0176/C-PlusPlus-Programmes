fee5c3c5a2dadf5095f13ef722d55d22c7b29551
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 22:04:57 +0600

    preprocessor created

--- Preprocessor/compiler-control-directives5.cpp
+++ Preprocessor/compiler-control-directives5.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include<conio.h>
+#define MAX 1
+int main()
+{
+    int a=10,b=20,m;
+    #if MAX==1
+    cout<<(a>b?a:b);
+    #else
+    cout<<(a<b?a:b);
+    #endif
+    return 0;
+}
