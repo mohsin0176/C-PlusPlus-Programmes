5b3ce9ac5360f0e4ce2309a605842ffcf01f1bc0
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 22:59:46 +0600

    I/O created

--- In-Out-Manipulators/use of dec,oct,hex.cpp
+++ In-Out-Manipulators/use of dec,oct,hex.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include<conio.h>
+#include<iomanip.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<dec<<n;
+    cout<<oct<<n;
+    cout<<hex<<n;
+    return 0;
+}
