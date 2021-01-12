2dedfcd5309bcd1f16f04a0392a0b763a30b4643
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 22:35:36 +0600

    I/O created

--- In-Out-Manipulators/chained calling of cout.write().cpp
+++ In-Out-Manipulators/chained calling of cout.write().cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char nm[25];
+    cin.getline(nm,25);
+    cout.write(7).write(nm,strlen(nm));
+    cout<<endl;
+    return 0;
+}
