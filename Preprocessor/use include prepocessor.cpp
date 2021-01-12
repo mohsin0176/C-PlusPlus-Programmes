ada285593ce49251a1eeb5fac0621051dbdf276a
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-12 21:42:25 +0600

    preprocessor created

--- Preprocessor/use include prepocessor.cpp
+++ Preprocessor/use include prepocessor.cpp
@@ -1,1 +1,35 @@
 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+Coding [myheader.h]:
+    int add(int x,int y)
+    {
+        int s;
+        s=x+y;
+        return s;
+
+    }
+
+    int sub(int x,int y)
+    {
+        int s;
+        d=x-y;
+        return d;
+
+    }
+
+    Coding:
+        #include<iostream>
+        #include<conio.h>
+        #include "myheader.h"
+        int main()
+        {
+            int a=20,b=10,res;
+            res=add(a,b);
+            cout<<res;
+            res=sub(a,b);
+            cout<<res;
+
+            return 0;
+        }
--- Preprocessor/use of -line-file-date-time.cpp
+++ Preprocessor/use of -line-file-date-time.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout<<__LINE__<<endl;
+     cout<<__FILE__<<endl;
+      cout<<__DATE__<<endl;
+       cout<<__TIME__<<endl;
+    return 0;
+}
