bbf1228ccaeb32155bd57cf59b6e4d97a3ea86ec
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 17:10:39 +0600

    VF created

--- Virtual-Functions/dynamic-late-runtime-binding.cpp
+++ Virtual-Functions/dynamic-late-runtime-binding.cpp
@@ -1,1 +1,55 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Shape
+{
+protected:
+    float width,height;
+public:
+    Shape(float a=0.0,float b=0.0)
+    {
+        width=a;
+        height=b;
+    }
+    virtual void area()
+    {
+        cout<<"Class Area"<<0;
+    }
+};
+class Rectangle :public Shape
+{
+    public:
+        Rectangle(float a=0.0,float b=0.0)
+{
+
+}
+void area()
+{
+    cout<<width*height;
+}
+};
+class Triangle :public Shape
+{
+    public:
+        Triangle(float a=0.0,float b=0.0)
+{
+
+}
+void area()
+{
+    cout<<width*height*.5;
+}
+};
+
+int main()
+{
+    Shape *shape;
+    Rectangle rec(10,7);
+    Triangle tri(10,5);
+    shape =&rec;
+    shape->area();
+    shape=&tri;
+    shape->area();
+    return 0;
+}
 
