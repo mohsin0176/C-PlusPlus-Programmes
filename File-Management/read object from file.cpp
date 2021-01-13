349de5cca0911b0aa4e8931c6212a4ef08c1fbf7
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 20:22:17 +0600

    File created

--- File-Management/read object from file.cpp
+++ File-Management/read object from file.cpp
@@ -1,1 +1,28 @@
-
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+using namespace std;
+class Person
+{
+    char name[25];
+    int age;
+public:
+    void getData()
+    {
+        cin.getline(name,25);
+        cin>>age;
+    }
+    void showData()
+    {
+        cout<<name<<age;
+    }
+};
+int main()
+{
+    Person p;
+    ifstream in("person.txt");
+    in.read((char*)&p,sizeof(p));
+    p.showData();
+    in.close();
+    return 0;
+}
--- File-Management/read specific record from file.cpp
+++ File-Management/read specific record from file.cpp
@@ -1,1 +1,38 @@
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+using namespace std;
+class Person
+{
+    char name[25];
+    int age;
+public:
+    void getData()
+    {
+        cin.getline(name,25);
+        cin>>age;
+    }
+    void showData()
+    {
+        cout<<name;
+        cout<<age;
+    }
+};
+int main()
+{
+    char ch;
+    Person p;
+    ifstream in;
+    in.open("person.txt");
 
+    in.seekg(0,ios::end);
+    int size=in.tellg();
+    int n=size/sizeof(p);
+    cin>>n;
+    int pos=(n-1)*sizeof(p);
+    in.seekg(pos);
+    in.read((char*)&p,sizeof(p));
+    p.showData();
+    in.close();
+    return 0;
+}
