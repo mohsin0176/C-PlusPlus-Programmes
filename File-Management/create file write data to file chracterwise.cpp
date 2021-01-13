2c209450a87299445a7c1e8e1441bef0d0dc3533
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 19:51:14 +0600

    File created

--- File-Management/create file write data to file chracterwise.cpp
+++ File-Management/create file write data to file chracterwise.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+#include<fstream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char str[50]="C++ Programming Example";
+    ofstream out("my.txt");
+    int i;
+    for(i=0;str[i]!='\n';i++)
+    {
+        out.put(str[i]);
+    }
+    cout<<"Data Written";
+    out.close();
+    return 0;
+}
--- File-Management/open file and read data characterwise.cpp
+++ File-Management/open file and read data characterwise.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+#include<fstream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char ch;
+    ifstream in("file.txt");
+    while(in)
+    {
+        in.get(ch);
+        cout<<ch;
+    }
+    in.close();
+    return 0;
+}
