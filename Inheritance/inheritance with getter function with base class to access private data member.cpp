418ab411d1fa547b1d1aa9bd01d269e8863d65f6
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-14 00:12:20 +0600

    inheritance created

--- Inheritance/inheritance with getter function with base class to access private data member.cpp
+++ Inheritance/inheritance with getter function with base class to access private data member.cpp
@@ -1,1 +1,61 @@
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+using namespace std;
+class Circle
+{
+private:
+    float radius;
+public:
+    void readRadius()
+    {
+        cin>>radius;
+    }
+    void showRadius()
+    {
+        cout<<radius;
+    }
+    float area()
+    {
+        float ar;
+        ar=PI*radius*radius;
+        return ar;
+    }
+    float getRadius()
+    {
+        return radius;
+    }
+};
+class Cylinder:private Circle
+{
+private:
+    float height;
+public:
+    void readHeight()
+    {
+        readRadius();
+        cin>>height;
+    }
+    void showHeight()
+    {
+        showRadius();
+        cout<<height;
+    }
+    float volume()
+        {
+            float v;
+            v=PI*getRadius()*getRadius()*height;
+            return v;
+        }
+};
+
+int main()
+{
+    Cylinder cl1;
+    cl1.readHeight();
+    cl1.showHeight();
+    float v=cl1.volume();
+    cout<<v;
+    return 0;
+}
 
