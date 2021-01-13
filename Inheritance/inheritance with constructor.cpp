6d4e71c8a57e4e8ae3f5de78425e3624b948cd68
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 23:44:32 +0600

    inheritance created

--- Inheritance/inheritance with constructor.cpp
+++ Inheritance/inheritance with constructor.cpp
@@ -1,1 +1,46 @@
 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Counter
+{
+protected:
+    unsigned int count;
+public:
+    Counter()
+    {
+        count=0;
+    }
+    Counter(int cnt)
+    {
+        count=cnt;
+    }
+    int getCount()
+    {
+        return count;
+    }
+    Counter operator++()
+    {
+        return Counter(++count);
+    }
+};
+class CountDn:public Counter
+{
+    public:
+    CountDn(){}
+    CountDn(int c){}
+    Counter operator--()
+    {
+        return Counter(--count);
+    }
+};
+int main()
+{
+    CountDn c1;
+    cout<<c1.getCount();
+    ++c1;++c1;++c1;
+    cout<<c1.getCount();
+    --c1;--c1;--c1;
+    cout<<c1.getCount();
+    return 0;
+}
