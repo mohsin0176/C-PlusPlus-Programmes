f71bfdc27757497c2be155b7941ad2af96f9c21b
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 19:35:41 +0600

    File created

--- File-Management/create file-write string-text line to file.cpp
+++ File-Management/create file-write string-text line to file.cpp
@@ -1,1 +1,18 @@
+#include <fstream>
+#include<iostream>
+#include<conio.h>
+#include<string.h>
 
+using namespace std;
+int  main()
+{
+    int x;
+    ofstream out("test.txt");
+    cin>> x;
+    out<<x<<endl;
+    out<<"C++ Programming Example\n";
+    out<<"Nil TechSys\n";
+    cout<<"Data Written";
+    out.close();
+    return 0;
+}
