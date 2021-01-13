919f778fc460e412d45c114f6271c93bd49e356b
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-14 00:03:25 +0600

    inheritance created

--- Inheritance/inheritance with default - parameterized constructor in base and derived class explicit call to base-class-parameterized-constructor.cpp
+++ Inheritance/inheritance with default - parameterized constructor in base and derived class explicit call to base-class-parameterized-constructor.cpp
@@ -1,1 +1,87 @@
+#include<iostream>
+#include<conio.h>
+#define PI 3.1416
+using namespace std;
+class Circle
+{
+protected:
+    float radius;
+public:
+    Circle()
+    {
+        radius=0;
+        cout<<"Default Constructor";
+    }
+    Circle(float r)
+    {
+        radius=r;
+        cout<<" Parameterized Constructor";
+    }
+    void readRadius()
+    {
+        cin>>radius;
+    }
+    void showRadius()
+    {
+        cout<<radius;
+    }
+    float area()
+    {
+        float ar;
+        ar=PI*radius*radius;
+        return ar;
+    }
+    float getRadius()
+    {
+        return radius;
+    }
+};
+class Cylinder:public Circle
+{
+private:
+    float height;
+public:
+    Cylinder()
+    {
+        height=0;
+        cout<<"Default Constructor";
+    }
+    Cylinder(float r,float h)
+    {
+        height=h;
+        cout<<"Parameterized Constructor";
+    }
+    void readHeight()
+    {
+        readRadius();
+        cin>>height;
+    }
+    void showHeight()
+    {
+        showRadius();
+        cout<<height;
+    }
+    float volume()
+        {
+            float v;
+            v=PI*radius*radius*height;
+            return v;
+        }
+};
+
+int main()
+{
+    Cylinder cl1(10,10);
+    cl1.showRadius();
+    cl1.showHeight();
+    cl1.readRadius();
+    cl1.readHeight();
+    cl1.showRadius();
+    cl1.showHeight();
+    float v=cl1.volume();
+    cout<<v;
+    return 0;
+}
+
+
 
