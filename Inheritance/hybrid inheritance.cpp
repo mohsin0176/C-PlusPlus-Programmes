b3e971788c0058ca6d7907f9bcec2294794cf851
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 22:19:42 +0600

    inheritance created

--- Inheritance/hybrid inheritance.cpp
+++ Inheritance/hybrid inheritance.cpp
@@ -1,1 +1,47 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int a,b,c,d,e;
+class A
+{
+public:
+    void getab()
+    {
+        cin>>a;
+        cin>>b;
+    }
+};
+class B:public A
+{
+public:
+    void getc()
+    {
+        cin>>c;
+    }
+};
+class C
+{
+public:
+    void getd()
+    {
+        cin>>d;
+    }
+};
+class D:public B,public C
+{
+public:
+    void result()
+    {
+        getab();
+        getc();
+        getd();
+        e=a+b+c+d;
+        cout<<e;
+    }
+};
+int main()
+{
+    D d1;
+    d1.result();
+    return 0;
+}
