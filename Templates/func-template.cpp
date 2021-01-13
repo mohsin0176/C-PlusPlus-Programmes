17ed2eac42bf3796b1144eaf3327db0b1c592b73
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 18:04:54 +0600

    Template created

--- Templates/func-template.cpp
+++ Templates/func-template.cpp
@@ -1,1 +1,19 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+template <class T>
+void swap(T a,T b)
+{
+    T t;
+    t=a;
+    a=b;
+    b=t;
+    cout<<a<<b;
+}
+int main()
+{
+    swap(10,20);
+    swap(10.25,20.25);
+    swap('A','B');
+    return 0;
+}
