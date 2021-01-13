ff9557582ace832824586639c046adf05bc9b35d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 22:12:38 +0600

    inheritance created

--- Inheritance/hierarchical inheritance.cpp
+++ Inheritance/hierarchical inheritance.cpp
@@ -1,1 +1,38 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+public:
+    int a,b;
+    void getnumber()
+    {
+        cin>>a;
+    }
+};
+class B:public A
+{
+public:
+    void square()
+    {
+        getnumber();
+        cout<<a*a;
+    }
+};
+class C:public A
+{
+public:
+    void cube()
+    {
+        getnumber();
+        cout<<a*a*a;
+    }
+};
+int main()
+{
+    B b1;
+    b1.square();
+    C c1;
+    c1.cube();
+    return 0;
+}
--- Inheritance/hierarchical inheritance1.cpp
+++ Inheritance/hierarchical inheritance1.cpp
@@ -1,1 +1,62 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+    int x;
+public:
+
+    void getX()
+    {
+        cin>>x;
+    }
+    void showX()
+    {
+        cout<<x;
+    }
+};
+class B:public A
+{
+int y;
+public:
+
+    void getY()
+    {
+        cin>>y;
+    }
+    void showY()
+    {
+        cout<<y;
+    }
+};
+class C:public A
+{
+int z;
+public:
+
+    void getZ()
+    {
+        cin>>z;
+    }
+    void showZ()
+    {
+        cout<<z;
+    }
+};
+int main()
+{
+    B b1;
+    C c1;
+ b1.getX();
+ b1.getY();
+ c1.getX();
+ c1.getZ();
+ b1.showX();
+ b1.showY();
+ c1.showX();
+ c1.showZ();
+
+
+    return 0;
+}
 
