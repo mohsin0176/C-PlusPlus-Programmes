bb79c28970c6563d92bc34632faaacb15d8d5a90
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 19:45:01 +0600

    File created

--- File-Management/open file read text from file.cpp
+++ File-Management/open file read text from file.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char str[50];
+    ifstream in("filename.txt");
+    while(in)
+    {
+        in.getline(str,50);
+        cout<<str<<endl;
+    }
+    in.close();
 
+    return 0;
+}
