ab1c4b6d3086bd969abc4ee24cd6ddcf37adccdc
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 18:02:24 +0600

    Template created

--- Templates/simple fun temp-generic-func.cpp
+++ Templates/simple fun temp-generic-func.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+template <class T>
+void show (T a)
+{
+    cout<<a;
+}
+int main()
+{
+    show('N');
+    show(10);
+    show(12.34);
+    show('NIls');
+    return 0;
+}
