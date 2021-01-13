25ec68d17212129bd8b99e028f7a3ac2bf7a627c
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-13 23:14:30 +0600

    inheritance created

--- Inheritance/multilevel inheritance.cpp
+++ Inheritance/multilevel inheritance.cpp
@@ -1,1 +1,45 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class top
+{
+public:
+    int a;
+    void getdata()
+    {
+        cin>>a;
+    }
+    void putdata()
+    {
+        cout<<a;
+    }
+};
+class middle: public top
+{
+public:
+    int b;
+    void square()
+    {
+        getdata();
+        b=a*a;
+        cout<<b;
+    }
+};
+class bottom : public middle
+{
+public:
+    int c;
+    void cube()
+    {
+        square();
+        c=b*a;
+        cout<<c;
+    }
+};
+int main()
+{
 
+    bottom b1;
+    b1.cube();
+    return 0;
+}
--- Inheritance/multilevel inheritance1.cpp
+++ Inheritance/multilevel inheritance1.cpp
@@ -1,1 +1,56 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class A
+{
+    int x;
+public:
+    void getX()
+    {
+        cin>>x;
+    }
+    void showX()
+    {
+        cout<<x;
+    }
+};
+class B :public A
+{
 
+    int y;
+public:
+    void getY()
+    {
+        cin>>y;
+    }
+    void showY()
+    {
+        cout<<y;
+    }
+};
+class C :public B
+{
+
+    int z;
+public:
+    void getZ()
+    {
+        cin>>z;
+    }
+    void showZ()
+    {
+        cout<<z;
+    }
+};
+
+int main()
+{
+    C c1;
+    c1.getX();
+    c1.getY();
+    c1.getZ();
+    c1.showX();
+    c1.showY();
+    c1.showZ();
+    return 0;
+}
