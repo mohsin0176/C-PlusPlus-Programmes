40285985d363a2a453a2871bfadbce91e79bdc54
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-14 17:15:39 +0600

    Pointer created

--- Pointers/simple-pointer-variable.cpp
+++ Pointers/simple-pointer-variable.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
 
+    int a=10;
+    int *p;
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    cout<<p<<endl;
+    cout<<*p<<endl;
+    cout<<&p<<endl;
+    return 0;
+}
