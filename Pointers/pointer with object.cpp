8a8d490d82b4d48ccc8c3962285a319db39a2c0d
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-14 17:45:21 +0600

    Pointer created

--- Pointers/pointer with object.cpp
+++ Pointers/pointer with object.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class B
+{
+public:
+    void display()
+    {
+        cout<<"Content of Base Class";
+    }
+};
+class D:public B
+{
+public:
+    void display()
+    {
+        cout<<"Content of Derived Class";
+    }
+};
+int main()
+{
+    B *b;
+    D d;
+    b=&d;
+    b->display();
 
+    return 0;
+}
