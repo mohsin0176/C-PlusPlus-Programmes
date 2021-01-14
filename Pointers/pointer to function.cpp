1c7f68b92c4d8f09b3733042135aea348b972ef8
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-14 18:22:02 +0600

    Pointer created

--- Pointers/pointer to function.cpp
+++ Pointers/pointer to function.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void Add(int,int);
+void Sub(int,int);
+int main()
+{
+    int a=10,b=20;
+    void (*pf)(int,int);
+    pf=Add;
+    (*pf)(a,b);
+    pf=Sub;
+    (*pf)(a,b);
+    return 0;
+}
+void Add(int x,int y)
+{
+    int s;
+    s=x+y;
+    cout<<s;
+}
+void Sub(int x,int y)
+{
+    int d;
+    d=x-y;
+    cout<<d;
+}
