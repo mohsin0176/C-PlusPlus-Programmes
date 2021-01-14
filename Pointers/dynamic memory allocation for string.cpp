b07fe56a1ebfa31aad74958b77a7e7b4f54a2cbd
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-14 19:08:02 +0600

    Pointer created

--- Pointers/dynamic memory allocation for string.cpp
+++ Pointers/dynamic memory allocation for string.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char s1[25]="C++ Programming Example";
+    int len=strlen(s1);
+    char *ps;
+    ps=new char[len+1];
+    strcpy(ps,s1);
+    cout<<ps;
+    delete ps;
+    return 0;
+}
--- Pointers/dynamic memory allocation for string1.cpp
+++ Pointers/dynamic memory allocation for string1.cpp
@@ -1,1 +1,31 @@
 
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+
+class String
+{
+    char *ps;
+public:
+    String(char s[])
+    {
+        int len=strlen(s);
+        ps=new char[len+1];
+        strcpy(ps,s);
+    }
+    ~String()
+    {
+        delete ps;
+    }
+    void display()
+    {
+        cout<<ps;
+    }
+};
+int main()
+{
+    String s1="NILS";
+    s1.display();
+    return 0;
+}
