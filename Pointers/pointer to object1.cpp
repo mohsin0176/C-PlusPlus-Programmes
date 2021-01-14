8b6d9c39fe2e19215052953b0e5fc69d3736e5ad
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-14 18:14:33 +0600

    Pointer created

--- Pointers/pointer to object1.cpp
+++ Pointers/pointer to object1.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class Distance
+{
+private:
+    int feet;
+    float inches;
+public:
+    void getDist()
+    {
+        cin>>feet>>inches;
+    }
+    void showDist()
+    {
+        cout<<feet<<inches;
+    }
+};
+int main()
+{
+    Distance *p;
+    p=new Distance;
+    p->getDist();
+    p->showDist();
+    return 0;
+}
 
