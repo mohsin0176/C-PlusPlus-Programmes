4b062e0f1e3ecc73084c1afa77693f836b60ab87
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-14 17:23:47 +0600

    Pointer created

--- Pointers/array and pointer.cpp
+++ Pointers/array and pointer.cpp
@@ -1,1 +1,24 @@
-
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a=10,b=20,c=30;
+    int *p[3],*t;
+    int i;
+    p[0]=&a;
+    p[1]=&b;
+    p[2]=&c;
+    for(i=0;i<3;i++)
+    {
+        cout<<*p[i];
+    }
+    t=p[0];
+    p[0]=p[2];
+    p[2]=t;
+for(i=0;i<3;i++)
+    {
+        cout<<*p[i];
+    }
+    return 0;
+}
--- Pointers/array of pointer.cpp
+++ Pointers/array of pointer.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a[]={10,20,30,40,50,60};
+    int *p;
+    int i;
+    p=&a[0];
+    for(i=0;i<5;i++)
+    {
+        cout<<a[i];
+    }
+    for(i=0;i<5;i++)
+    {
+        cout<<i[a];
+    }
+    for(i=0;i<5;i++)
+    {
+        cout<<*(a+i);
+    }
+    for(i=0;i<5;i++)
+    {
+        cout<<*(p+i);
 
+    }
+    for(i=0;i<5;i++)
+    {
+        cout<<p[i];
+    }
+    for(i=0;i<5;i++)
+    {
+        cout<<*p++;
+    }
+    return 0;
+}
